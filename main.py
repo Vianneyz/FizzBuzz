1be01644b6a165cd9bcd1c649c321435602cbbc9
Azmogi <55248556+Azmogi@users.noreply.github.com>
2021-02-25 16:59:46 +0100

    Ajout champ saisie

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-I = -4
-while I < 101:
-    if I < 1:
-        print("Erreur : chiffre invalide")
-    elif I % 3 == 0 and I % 5 == 0:
-        print("FizzBuzz")
-    elif I % 3 == 0:
-        print("Fizz")
-    elif I % 5 == 0:
-        print("Buzz")
-    else:
-        print(I)
-    I += 1
+I = int(input("Entrez un chiffre :"))
+if I < 1:
+    print("Erreur : chiffre invalide")
+elif I % 3 == 0 and I % 5 == 0:
+    print("FizzBuzz")
+elif I % 3 == 0:
+    print("Fizz")
+elif I % 5 == 0:
+     print("Buzz")
+else:
+    print(I)
